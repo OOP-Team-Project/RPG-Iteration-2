7caa9075711f20a5e501f3226114a2762eb04d7e
Joshua Nassar <joshnassar94@gmail.com>
2016-02-27 18:52:36 -0500

    Basic structure/functionality for Vehicle and Pet

--- src/com/TigersIter2/entities/Pet.java
+++ src/com/TigersIter2/entities/Pet.java
@@ -4,6 +4,11 @@ package com.TigersIter2.entities;
  * Created by Josh on 2/27/2016.
  */
 public class Pet extends Entity {
+    private String name;
+
+    public Pet(String n){
+        name = n;
+    }
 
     @Override
     public void update(int xMovement, int yMovement) {
--- src/com/TigersIter2/entities/Vehicle.java
+++ src/com/TigersIter2/entities/Vehicle.java
@@ -4,13 +4,29 @@ package com.TigersIter2.entities;
  * Created by Josh on 2/27/2016.
  */
 public class Vehicle extends Entity {
+    private String name;
+    private int movementBonus;
+    private boolean canPassWater;
+    private boolean canPassMountain;
+
+    public Vehicle(String name, int movement, boolean water, boolean mountain){
+        this.name = name;
+        this.movementBonus = movement;
+        this.canPassWater = water;
+        this.canPassMountain = mountain;
+    }
+
+    public boolean getCanPassWater(){
+        return canPassWater;
+    }
+
+    public boolean getCanPassMountain(){
+        return canPassMountain;
+    }
+
 
     @Override
     public void update(int xMovement, int yMovement) {
         System.out.println(xMovement + ", " + yMovement);
-    }
-
-    public int getDirection(){
-        return 0;   //until direction is implemented
     }
 }
