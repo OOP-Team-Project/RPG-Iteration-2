efcef277b9a3260adff89dd2a1ec6a891bbe006f
slichtenheld <slichtenheld@ufl.edu>
2016-02-27 15:35:57 -0500

    added new folders, that I forgot to commit before

---
+++ src/com/TigersIter2/assets/sprites/TerrainSprite.java
@@ -0,0 +1,23 @@
+package com.TigersIter2.assets.sprites;
+
+import com.TigersIter2.assets.StaticVar;
+
+import java.awt.image.BufferedImage;
+
+//static way to retrieve Terrain images(SL)
+//for ex. TerrainSprite.mountain returns mountain pic
+public class TerrainSprite {
+
+    public static BufferedImage mountain, water, grass;
+
+    //needs to be called once
+    public static void init(){
+        SpriteSheet terrainSheet = new SpriteSheet(SpriteLoader.loadImage("/textures/terrains.png"));
+
+        mountain = terrainSheet.cropSheet(0,0, StaticVar.terrainImageWidth, StaticVar.terrainImageHeight);
+        water = terrainSheet.cropSheet(StaticVar.terrainImageWidth*1, 0, StaticVar.terrainImageWidth, StaticVar.terrainImageHeight);
+        grass = terrainSheet.cropSheet(StaticVar.terrainImageWidth*2, 0, StaticVar.terrainImageWidth, StaticVar.terrainImageHeight);
+
+    }
+
+}
