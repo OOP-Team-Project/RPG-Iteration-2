a2de158e17fe1ed8bc163fd8d85f62c4230ee9d4
slichtenheld <slichtenheld@ufl.edu>
2016-02-29 14:21:05 -0500

    better hexagonal shape

--- src/com/TigersIter2/assets/StaticVar.java
+++ src/com/TigersIter2/assets/StaticVar.java
@@ -9,7 +9,7 @@ public class StaticVar {
 
     public static final int fps = 60;
 
-    public static final int terrainImageHeight = 96;    //Was 64 before evil Miles Came
+    public static final int terrainImageHeight = 80;    //Was 64 before evil Miles Came  //use 80 for hexTerrainsTemp
     public static final int terrainImageWidth = 96;     //Was 64 before evil Miles Came
 
     public static final int entityImageHeight = 128;
