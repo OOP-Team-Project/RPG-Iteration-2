ebc7cc4d2757f913db150106c4a681fc6498fe5e
slichtenheld <slichtenheld@ufl.edu>
2016-03-06 21:55:10 -0500

    added some BS for loading in maps, -> still needs a lot of work

--- src/com/TigersIter2/maps/TerrainMap.java
+++ src/com/TigersIter2/maps/TerrainMap.java
@@ -15,30 +15,51 @@ import java.util.ArrayList;
 public class TerrainMap implements Map{
 
     public static int heightOfWorld, widthOfWorld;
-    private int[][] terrainMap;
+    public int[][] terrainMap;
 
     //Do we need a list for tiles? Wouldn't a 2D array work just fine since the size will be fixed upon initialization? - Sam
     ArrayList<ArrayList<TerrainType>> tiles = new ArrayList<ArrayList<TerrainType>>();  //Don't be intimidated! This is just a 2D Array pretty much
-
+    private TerrainType[][] tilesArray;
 
 
     public TerrainMap(){   //Default Constructor, uh... makes a bunch of grass tiles and stuff? I guess?
+//        for(int i = 0; i < 40; i++){
+//            tiles.add(new ArrayList<TerrainType>());
+//            for(int j = 0; j < 40; j++){
+//                if((i == 0 || i == 1))
+//                    tiles.get(i).add(new Mountain());
+//                else if(i == 38 || i == 39)
+//                    tiles.get(i).add(new Mountain());
+//                else if (j == 0 || j == 1)
+//                    tiles.get(i).add(new Mountain());
+//                else if (j == 38 || j == 39)
+//                    tiles.get(i).add(new Mountain());
+//                else if((j % 5 == 0))
+//                    tiles.get(i).add(new Water());
+//                else
+//                    tiles.get(i).add(new Grass());
+//            }
+//        }
+        tilesArray = new TerrainType[40][40];
         for(int i = 0; i < 40; i++){
-            tiles.add(new ArrayList<TerrainType>());
             for(int j = 0; j < 40; j++){
                 if((i == 0 || i == 1))
-                    tiles.get(i).add(new Mountain());
+                    tilesArray[i][j] = (new Mountain());
                 else if(i == 38 || i == 39)
-                    tiles.get(i).add(new Mountain());
+                    tilesArray[i][j]=(new Mountain());
                 else if (j == 0 || j == 1)
-                    tiles.get(i).add(new Mountain());
+                    tilesArray[i][j]=(new Mountain());
                 else if (j == 38 || j == 39)
-                    tiles.get(i).add(new Mountain());
+                    tilesArray[i][j]=(new Mountain());
                 else if((j % 5 == 0))
-                    tiles.get(i).add(new Water());
+                    tilesArray[i][j]=(new Water());
                 else
-                    tiles.get(i).add(new Grass());
+                    tilesArray[i][j]=(new Grass());
             }
+        }
+        for(int i = 0; i < 40; i++) {
+            tiles.add(new ArrayList<TerrainType>());
+            for (int j = 0; j < 40; j++) tiles.get(i).add(tilesArray[i][j]);
         }
         
         // // TODO: 3/6/2016 Sam -> move this into helper/association class or somehow clean up 
@@ -68,7 +89,7 @@ public class TerrainMap implements Map{
         return tiles;
     }
 
-    public void setTerrainTypes(ArrayList<ArrayList<TerrainType>> tiles) {
-        this.tiles = tiles;
-    }
+//    public void setTerrainTypes(ArrayList<ArrayList<TerrainType>> tiles) {
+//        this.tiles = tiles;
+//    }
 }
