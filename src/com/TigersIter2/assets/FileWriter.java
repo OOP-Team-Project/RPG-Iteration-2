9ae38d0d357e290a734cabd9589a40153d06aad2
slichtenheld <slichtenheld@ufl.edu>
2016-03-12 14:40:48 -0500

    added FileWriter

---
+++ src/com/TigersIter2/assets/FileWriter.java
@@ -0,0 +1,28 @@
+package com.TigersIter2.assets;
+
+import java.io.*;
+
+/**
+ * Created by slichtenheld on 3/12/2016.
+ */
+public class FileWriter {
+
+    public static void stringToFile(String fileName, String fileContent){
+
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(fileName), "utf-8"))){
+            writer.write(fileContent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static String intToString(int val){
+        try{
+            return Integer.toString(val);
+        }catch(NumberFormatException e){
+            e.printStackTrace();
+            return "0";
+        }
+    }
+}
+
