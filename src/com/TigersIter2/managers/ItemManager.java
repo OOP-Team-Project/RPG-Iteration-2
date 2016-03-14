c4465c5b8480d16aa5c6d041f006cdc576760978
slichtenheld <slichtenheld@ufl.edu>
2016-03-14 04:53:08 -0400

    pre merge commit

--- src/com/TigersIter2/managers/ItemManager.java
+++ src/com/TigersIter2/managers/ItemManager.java
@@ -30,6 +30,65 @@ public class ItemManager {
         //avatar.getInventory().getItems();
         loadItems();
     }
+    public void addItem(Item item) {
+        itemList.add(item);
+        item.setDisplay(true);
+    }
+
+    public List<Item> getItemList(){
+        return itemList;
+    }
+
+    /*checks the tile player is moving to to see if there is an item on it
+     *Handles the interaction based off the item type, returns false if item is
+     *an obstacle*/
+    public boolean checkTile(long elapsed, int xMov, int yMov) {
+        Iterator<Item> iter = itemList.iterator();
+        Location nextLocation = new Location(0, 0, 0);
+        nextLocation.setX(avatar.getLocation().getX());
+        nextLocation.setY(avatar.getLocation().getY());
+        nextLocation.incrementX(Math.round(xMov * elapsed * StaticVar.entitySpeed*avatar.getStats().getMovement()));
+        nextLocation.incrementY(Math.round(yMov * elapsed * StaticVar.entitySpeed*avatar.getStats().getMovement()));
+        while(iter.hasNext()) {
+            Item item = iter.next();
+            if(item.getDisplay()) {
+                if (LocationConverter.PixelLocationToHex(item.getLocation()).getX() == LocationConverter.PixelLocationToHex(nextLocation).getX() &&
+                        LocationConverter.PixelLocationToHex(item.getLocation()).getY() == LocationConverter.PixelLocationToHex(nextLocation).getY()) {
+
+                    if (item instanceof TakeableItem) {
+                        avatarInventory.addItem((TakeableItem) item);
+                        item.setDisplay(false);
+                        return true;
+                    } else if (item instanceof OneShot) {
+                        playerStats.addStatModifier(((OneShot) item).getStatsModifier());
+                        item.setDisplay(false);
+                        return true;
+                    } else if (item instanceof Interactive) {
+                        List<TakeableItem> avatarInventoryItems = avatarInventory.getItems();
+                    /* Iterates through the player's inventory to see if it holds the corresponding Key */
+                        for (TakeableItem i : avatarInventoryItems) {
+                            if (i instanceof Key) {
+                                if (!((Interactive) item).getInteractedWith()) { //keys match!
+                                    if (((Key) i).getItemCode() == ((Interactive) item).getItemCode()) {
+                                        ((Interactive) item).setInteractedWith(true);
+                                        return true;
+                                    }
+                                }
+                            }
+                        }
+                        if(!((Interactive) item).getInteractedWith())
+                            return false;
+                    } else if (item instanceof Obstacle) { //item is an obstacle
+                        System.out.println("obstacle encountered");
+                        return false;
+                    }
+                }
+            }
+
+        }
+
+        return true;
+    }
 
     private void loadItems(){
         System.out.println("ItemManager loading in....");
@@ -240,63 +299,5 @@ public class ItemManager {
         }
     }
 
-    public void addItem(Item item) {
-        itemList.add(item);
-        item.setDisplay(true);
-    }
 
-    public List<Item> getItemList(){
-        return itemList;
-    }
-
-    /*checks the tile player is moving to to see if there is an item on it
-     *Handles the interaction based off the item type, returns false if item is
-     *an obstacle*/
-    public boolean checkTile(long elapsed, int xMov, int yMov) {
-        Iterator<Item> iter = itemList.iterator();
-        Location nextLocation = new Location(0, 0, 0);
-        nextLocation.setX(avatar.getLocation().getX());
-        nextLocation.setY(avatar.getLocation().getY());
-        nextLocation.incrementX(Math.round(xMov * elapsed * StaticVar.entitySpeed*avatar.getStats().getMovement()));
-        nextLocation.incrementY(Math.round(yMov * elapsed * StaticVar.entitySpeed*avatar.getStats().getMovement()));
-        while(iter.hasNext()) {
-            Item item = iter.next();
-            if(item.getDisplay()) {
-                if (LocationConverter.PixelLocationToHex(item.getLocation()).getX() == LocationConverter.PixelLocationToHex(nextLocation).getX() &&
-                        LocationConverter.PixelLocationToHex(item.getLocation()).getY() == LocationConverter.PixelLocationToHex(nextLocation).getY()) {
-
-                    if (item instanceof TakeableItem) {
-                        avatarInventory.addItem((TakeableItem) item);
-                        item.setDisplay(false);
-                        return true;
-                    } else if (item instanceof OneShot) {
-                        playerStats.addStatModifier(((OneShot) item).getStatsModifier());
-                        item.setDisplay(false);
-                        return true;
-                    } else if (item instanceof Interactive) {
-                        List<TakeableItem> avatarInventoryItems = avatarInventory.getItems();
-                    /* Iterates through the player's inventory to see if it holds the corresponding Key */
-                        for (TakeableItem i : avatarInventoryItems) {
-                            if (i instanceof Key) {
-                                if (!((Interactive) item).getInteractedWith()) { //keys match!
-                                    if (((Key) i).getItemCode() == ((Interactive) item).getItemCode()) {
-                                        ((Interactive) item).setInteractedWith(true);
-                                        return true;
-                                    }
-                                }
-                            }
-                        }
-                        if(!((Interactive) item).getInteractedWith())
-                            return false;
-                    } else if (item instanceof Obstacle) { //item is an obstacle
-                        System.out.println("obstacle encountered");
-                        return false;
-                    }
-                }
-            }
-
-        }
-
-        return true;
-    }
 }
