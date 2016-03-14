ae0f75f5df0892c023ca7dc8f22329d45bab8fe8
muletmiles <muletmiles@ufl.edu>
2016-03-13 20:56:52 -0400

    Fixed issue where scrolling was stuck at the edges of the map

--- src/com/TigersIter2/views/View.java
+++ src/com/TigersIter2/views/View.java
@@ -3,6 +3,7 @@ package com.TigersIter2.views;
 import com.TigersIter2.assets.StaticVar;
 import com.TigersIter2.entities.Avatar;
 import com.TigersIter2.location.Location;
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
 
 import javax.swing.*;
 
@@ -19,7 +20,7 @@ public class View extends JComponent {
     public static Location cameraLocation = new Location(0, 0, 0);
     public static EntityFollowingCamera eCam;
     public static Camera scrollCam = new Camera();
-    public static int mapXLength, mapYLength; //Hack, just added this to make things work with AttackIndicatorView (which, unfortunately, I don't have time to fully understand why that's static right now!)
+    public static int mapXLength, mapYLength;
 
     public static mapMode currentMapMode = mapMode.PLAYER_FOLLOW_MODE;
 
@@ -28,8 +29,23 @@ public class View extends JComponent {
             return;
 
         if(currentMapMode == mapMode.SCROLL_MODE){
-            scrollCam.setObjectOfInterest_X(scrollCam.getObjectOfInterest_X() + Math.round(xMovement * elapsed * StaticVar.cameraSpeed));
-            scrollCam.setObjectOfInterest_Y(scrollCam.getObjectOfInterest_Y() + Math.round(yMovement * elapsed * StaticVar.cameraSpeed));
+
+            int newX = scrollCam.getObjectOfInterest_X() + Math.round(xMovement * elapsed * StaticVar.cameraSpeed);
+            int newY = scrollCam.getObjectOfInterest_Y() + Math.round(yMovement * elapsed * StaticVar.cameraSpeed);
+
+            if(newX < StaticVar.terrainImageWidth*StaticVar.xTilesFromEdge)
+                newX = StaticVar.terrainImageWidth*StaticVar.xTilesFromEdge;
+            if(newX > StaticVar.terrainImageWidth*(mapXLength - StaticVar.xTilesFromEdge + 1))
+                newX = StaticVar.terrainImageWidth*(mapXLength - StaticVar.xTilesFromEdge + 1);
+
+            if(newY < StaticVar.terrainImageHeight*StaticVar.yTilesFromEdge)
+                newY = StaticVar.terrainImageHeight*StaticVar.yTilesFromEdge;
+            if(newY > StaticVar.terrainImageHeight*(mapYLength - StaticVar.yTilesFromEdge))
+                newY = StaticVar.terrainImageHeight*(mapYLength -StaticVar.yTilesFromEdge);
+
+
+            scrollCam.setObjectOfInterest_X(newX);
+            scrollCam.setObjectOfInterest_Y(newY);
 
             cameraLocation.setX(scrollCam.getObjectOfInterest_X());
             cameraLocation.setY(scrollCam.getObjectOfInterest_Y());
