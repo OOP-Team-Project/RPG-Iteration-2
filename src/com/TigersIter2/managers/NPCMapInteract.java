8f582a96b807f9810f1c81e5673c0abd261105a0
kulakowskin <kulakowskin@yahoo.com>
2016-03-13 21:06:17 -0400

    Working on NPC movement

--- src/com/TigersIter2/managers/NPCMapInteract.java
+++ src/com/TigersIter2/managers/NPCMapInteract.java
@@ -34,7 +34,16 @@ public class NPCMapInteract {
         if (terrain == 2 || terrain == 3){
             convertDegreesToCoord(randomDirection(npc));
             npc.update(xMov, yMov, elapsed);
-            npc.changeDirection(xMov, yMov);
+            // For the NPCs to chill for a bit after moving
+            new java.util.Timer().schedule(
+                    new java.util.TimerTask() {
+                        @Override
+                        public void run() {
+                            npc.changeDirection(xMov, yMov);
+                        }
+                    },
+                    2000
+            );
         }
         else if(terrain == 1){
             npc.update(xMov, yMov, elapsed);
